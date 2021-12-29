8f663178fa0519f70f0cc1929485a175d47ad624
JasonWu1111 <jasonwu19960210@gmail.com>
2021-12-29 11:46:41 +0800

    Update hightlight style to .

--- sw.js
+++ sw.js
@@ -21,8 +21,8 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   "./img/home-bg4.png",
   "./img/404-bg.jpg",
-  "./css/hux-blog.min.css",
-  "./css/bootstrap.min.css"
+  "./css/hux-blog.css",
+  "./css/bootstrap.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
